f2f4940657906821123039b5817b71d988f9e27f
GrantMelvin <92465197+GrantMelvin@users.noreply.github.com>
2022-11-25 22:03:39 -0500

    name change

--- assg1/ColorFinder.java
+++ assg1/ColorFinder.java
@@ -6,7 +6,7 @@ public class ColorFinder {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Please enter a sentence: ") ;
+		System.out.println("Please enter a sentence: ") ; 
 		
 		// Creates user input object
 		Scanner keyboard = new Scanner(System.in) ;
